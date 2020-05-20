269bf36f0fa76e1abae12fc9afab95a5b9c63093
Daniel Poliak <daniel.poliak@erstegroup.com>
2020-05-20 21:36:29 +0200

    remove comments

--- src/controllers/v0/index.router.ts
+++ src/controllers/v0/index.router.ts
@@ -5,8 +5,6 @@ const router: Router = Router();
 
 router.use('/', ImageRouter);
 
-// Root Endpoint
-// Displays a simple message to the user
 router.get( "/", async ( req:Request, res:Response ) => {
     res.send("try GET /filteredimage?image_url={{}}")
 } );
